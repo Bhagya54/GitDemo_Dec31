24b3e6ca31dd41aa6a91ead8ceda529466a08647
Bhagya54 <testingtrainer54@gmail.com>
2025-12-31 09:36:47 +0530

    one more line-Tester

--- src/test/java/day1/Demo2_AsTester.java
+++ src/test/java/day1/Demo2_AsTester.java
@@ -3,5 +3,6 @@ package day1;
 public class Demo2_AsTester {
     public static void main(String[] args) {
         System.out.println("Demo2 - As Tester");
+        System.out.println("one more syso - As Tester");
     }
 }
